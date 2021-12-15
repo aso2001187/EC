24ad20167822f42652bde32d1244c52a6342a55e
Aso2001170 <83050613+Aso2001170@users.noreply.github.com>
2021-12-15 16:58:35 +0900

    cartin UPDATEうまくかない

--- SlimeProject/php/cartin.php
+++ SlimeProject/php/cartin.php
@@ -1,18 +1,28 @@
 <?php
+session_start();
 $pdo=new PDO('mysql:host=mysql152.phy.lolipop.lan;
             dbname=LAA1291072-team;charset=utf8',
     'LAA1291072',
     'asot6');
-
-
-if(isset($_POST['item_id'])){
-    $sql=$pdo->prepare('insert into carton(ca_itemid,ca_id,ca_number) values (?,?,?)');
-    $sql->bindValue(1,$_SESSION['customer']['id']);
-    $sql->bindValue(2,$_POST['item_id']);
-    $sql->bindValue(3,$_SESSION['']);
+    $number = null;
+    $sql = $pdo->prepare('SELECT ca_itemid,ca_id,ca_number FROM carton WHERE ca_itemid = ?');
+    $sql->execute([$_POST['item_id']]);
+    foreach ($sql as $row){
+        $number = $row['ca_number'];
+    }
+    if(isset($number)){
+        $sql=$pdo->prepare('insert into carton(ca_itemid,ca_id,ca_number) values (?,?,?)');
+        $sql->bindValue(1,$_SESSION['customer']['id']);
+        $sql->bindValue(2,$_POST['item_id']);
+        $sql->bindValue(3,1);
+    }else{
+        $sql=$pdo->prepare('UPDATE carton SET ca_number = ? WHERE ca_id = ? and ca_itemid = ?');
+        $sql->bindValue(1,$number+1);
+        $sql->bindValue(2,$_SESSION['customer']['id']);
+        $sql->bindValue(3,$_POST['item_id']);
+    }
     $sql->execute();
     $sql = null;
     $pdo=null;
     require_once '../php/toppage.php';
-}
 ?>

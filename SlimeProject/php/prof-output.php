f68d9c6d3bb23bb6b3a9d6a8e50b3ffcad9461b1
aso2001187 <83051480+aso2001187@users.noreply.github.com>
2021-12-16 21:44:19 +0900

    Update prof-output.php

--- SlimeProject/php/prof-output.php
+++ SlimeProject/php/prof-output.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -14,61 +15,9 @@
 </head>
 <body>
 <!--ここから上部ヘッダー-->
-<header>
-    <div class="header_boss">
-        <!--ヘッダーの左寄せ部分-->
-        <div class="header_left">
-            <a href="0">logo<img src="0#"></a>
-        </div>
-        <!--ヘッダーの右寄せ部分-->
-        <ul class="header_right">
-            <!--検索ボックス-->
-            <li class="header_right_item">
-                <form method="post" action="#???" class="keyword"> <!--キーワード検索用form-->
-                    <div class="header_items3">
-                        <input type="text" id="search" placeholder="キーワード検索" class="keyword_box">
-                        <input type="submit" value="&#xf002" class="keyword_submit">
-                    </div>
-                </form> <!--キーワード検索用form ここまで-->
-            </li>
-            <!--ログインボタン-->
-            <li class="header_right_item">
-                <div class="header_items">
-                    <a href="login.html"><ion-icon name="person-outline" class="header_icon1"></ion-icon>
-                        <span>Login</span></a>
-                </div>
-            </li>
-            <!--カートボタン-->
-            <li class="header_right_item">
-                <div class="header_items2">
-                    <a href="#???"><ion-icon name="cart-outline" class="header_icon2"></ion-icon>
-                        <span>Cart</span></a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</header>
-<!--上部ヘッダー終わり-->
-
-<!--左サイドバー-->
-<div id="sidebar">
-    <div class="bg"></div>
-    <div class="sidebar-button" tabindex="0">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </div>
-    <div id="sidebar-menu" tabindex="0">
-        <ul>
-            <li><a href="#0">TOP</a></li>
-            <li><a href="#0">##TAG1</a></li>
-            <li><a href="#0">##TAG2</a></li>
-            <li><a href="#0">##TAG3</a></li>
-            <li><a href="#0">##TAG4...</a></li>
-            <li class="small"><a href="#0">Contact</a></li>
-        </ul>
-    </div>
-</div>
+<?php
+require 'parts.php';
+?>
 <!--サイドバー終わり-->
 
 <!--ここからメインエリア-->
@@ -80,22 +29,19 @@
         'LAA1291072',
         'asot6');
 
-    $sql = $pdo->prepare('update costomer set C_name = ?, C_postcode = ?, C_address1 = ?, C_address2 = ?, C_phone = ?, C_email = ? where C_id = ?');
+    $sql = $pdo->prepare('update customer set C_name = ?, C_postcode = ?, C_address1 = ?, C_address2 = ?, C_phone = ?, C_email = ? where C_id = ?');
 
-    $sql->bindValue(1,$_POST['C_name'], PDO::PARAM_STR);
-    $sql->bindValue(2,$_POST['C_postcode'], PDO::PARAM_STR);
-    $sql->bindValue(3,$_POST['C_address1'], PDO::PARAM_STR);
-    $sql->bindValue(4,$_POST['C_address2'], PDO::PARAM_STR);
-    $sql->bindValue(5,$_POST['C_phone'], PDO::PARAM_STR);
-    $sql->bindValue(6,$_POST['C_email'], PDO::PARAM_STR);
-    $sql->bindValue(7,$_POST['C_id'], PDO::PARAM_STR);
+    $sql->bindValue(1,$_POST['name'], PDO::PARAM_STR);
+    $sql->bindValue(2,$_POST['postcode'], PDO::PARAM_STR);
+    $sql->bindValue(3,$_POST['address1'], PDO::PARAM_STR);
+    $sql->bindValue(4,$_POST['address2'], PDO::PARAM_STR);
+    $sql->bindValue(5,$_POST['phone'], PDO::PARAM_STR);
+    $sql->bindValue(6,$_POST['email'], PDO::PARAM_STR);
+    $sql->bindValue(7,$_SESSION['customer']['id'], PDO::PARAM_STR);
     $sql->execute();
 
-    if($sql->rowCount()>0){
         echo '<p>更新完了しました</p>';
-    }else{
-        echo '<p>更新エラー</p>';
-    }
+        echo '<br><a href="toppage.php">トップページに戻る</a>';
     ?>
 
 </main>
